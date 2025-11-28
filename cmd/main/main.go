b5094c6d11fed27c2b8318c435ca18dad6ca707a
LeonibelDev <leonibel.ramirez@gmail.com>
2025-11-28 18:24:55 -0400

    update

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -6,27 +6,41 @@ import (
 	"os"
 	"strings"
 	"text/template"
+	"time"
 
 	"github.com/leonibeldev/url-shortener/internal/db"
 	"github.com/leonibeldev/url-shortener/internal/routes"
 )
 
+var tmpl = template.Must(template.ParseFiles("internal/front/index.html"))
+
+func Logger(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+		next.ServeHTTP(w, r)
+
+		logMessage := fmt.Sprintf("[%s] - %s %s - %v", r.Method, r.RequestURI, r.RemoteAddr, time.Since(start))
+		fmt.Println(logMessage)
+	})
+}
+
+func Home(w http.ResponseWriter, r *http.Request) {
+	if strings.TrimPrefix(r.URL.Path, "/") == "" {
+
+		tmpl.Execute(w, nil)
+	} else {
+		routes.HandleRedirect(w, r)
+	}
+}
+
 func main() {
 	// Validate DB
 	db.ValidateDBExists()
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		if strings.TrimPrefix(r.URL.Path, "/") == "" {
-			tmpl := template.Must(template.ParseFiles("internal/front/index.html"))
-
-			tmpl.Execute(w, nil)
-		} else {
-			routes.HandleRedirect(w, r)
-		}
-	})
-
 	http.HandleFunc("/shorten", routes.HandleShorten)
-	//http.HandleFunc("/r/", routes.HandleRedirect)
+
+	// Logger middleware
+	http.Handle("/", Logger(http.HandlerFunc(Home)))
 
 	PORT := os.Getenv("PORT")
 	if PORT == "" {
